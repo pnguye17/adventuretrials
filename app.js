f99dab7d134dbc3003fc2cced807367fdd7576c7
Peter Nguyen <peter.nguyen17@gmail.com>
2020-06-05 01:25:22 -0700

    added path/fs modules for servingtatic pages

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-console.log("hello")
-
 /**
  * Create simple server with http module
+ * Set up index page for client at / and have the web application display Hello World
  */
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const port = 3000
 
- const server = require('http')
- const port = 3000
- server.createServer().listen(port, () => console.log(`server is running on port: ${port}`))
+const server = http.createServer((req, res) => {
+     res.write("hello world")
+     res.end()
+    })
+     .listen(port, () => 
+     console.log(`server is running on port: ${port}`))
+
+
+
+
 
