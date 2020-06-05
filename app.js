d5fc2b765f2edcb995304f811e5b144120d177bb
Peter Nguyen <peter.nguyen17@gmail.com>
2020-06-05 00:37:27 -0700

    created server by http module

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("hello")
+
+/**
+ * Create simple server with http module
+ */
+
+ const server = require('http')
+ const port = 3000
+ server.createServer().listen(port, () => console.log(`server is running on port: ${port}`))
+
